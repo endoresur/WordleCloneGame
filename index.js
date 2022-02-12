2a6efe6965f90a74608affa4c3320dccd2678781
endoresur <mlhv2lv@gmail.com>
2022-02-12 23:28:58 +0800

    Add basic styles

---
+++ index.js
@@ -0,0 +1,7 @@
+const tileDisplay = document.querySelector('.tile-container');
+const keyBoard = document.querySelector('.key-container');
+
+const keys = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A',
+              'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'ENTER',  'Z',
+              'X', 'C', 'V', 'B', 'N', 'M', '«',
+]

a527323ce6bd480ce36db8f3f329f3d7638129e6
endoresur <mlhv2lv@gmail.com>
2022-02-13 00:50:26 +0800

    Add guess rows and styled them

--- index.js
+++ index.js
@@ -5,8 +5,29 @@ const keys = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A',
               'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'ENTER',  'Z',
               'X', 'C', 'V', 'B', 'N', 'M', '«'];
 
+const guessRows = [
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+]
+
+guessRows.forEach((guessRow, rowIndex) => {
+    const rowElement = document.createElement('div');
+    rowElement.setAttribute('id', 'guessRow-' + rowIndex);
+    guessRow.forEach((guess, guessIndex) => {
+        const tileElement = document.createElement('div');
+        tileElement.setAttribute('id',  'guessRow-' + rowIndex + '-tile-' + guessIndex);
+        tileElement.classList.add('tile');
+        rowElement.append(tileElement);
+    });
+    tileDisplay.append(rowElement);
+})
+
 const handleClick = () => {
-    
+
 }
 
 keys.forEach(key => {

7aa4b7c7b1d776729d566062c047df9858bc05dd
endoresur <mlhv2lv@gmail.com>
2022-02-13 12:54:00 +0800

    Add ability to add and delete letters

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const tileDisplay = document.querySelector('.tile-container');
 const keyboard = document.querySelector('.key-container');
 
+const wordle = 'SUPER';
+
 const keys = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A',
-              'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'ENTER',  'Z',
-              'X', 'C', 'V', 'B', 'N', 'M', '«'];
+    'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'ENTER', 'Z',
+    'X', 'C', 'V', 'B', 'N', 'M', '«'];
 
 const guessRows = [
     ['', '', '', '', ''],
@@ -12,28 +14,59 @@ const guessRows = [
     ['', '', '', '', ''],
     ['', '', '', '', ''],
     ['', '', '', '', ''],
-]
+];
+
+let currentRow = 0;
+let currentTile = 0;
 
 guessRows.forEach((guessRow, rowIndex) => {
     const rowElement = document.createElement('div');
     rowElement.setAttribute('id', 'guessRow-' + rowIndex);
     guessRow.forEach((guess, guessIndex) => {
         const tileElement = document.createElement('div');
-        tileElement.setAttribute('id',  'guessRow-' + rowIndex + '-tile-' + guessIndex);
+        tileElement.setAttribute('id', 'guessRow-' + rowIndex + '-tile-' + guessIndex);
         tileElement.classList.add('tile');
         rowElement.append(tileElement);
     });
     tileDisplay.append(rowElement);
 })
 
-const handleClick = () => {
-
-}
-
 keys.forEach(key => {
     const buttonElement = document.createElement('button');
     buttonElement.textContent = key;
     buttonElement.setAttribute('id', key);
-    buttonElement.addEventListener('click', handleClick)
+    buttonElement.addEventListener('click', () => handleClick(key));
     keyboard.append(buttonElement);
 })
+
+const handleClick = (key) => {
+    if (key === '«') {
+        deleteLetter(key);
+        return;
+    }
+    if (key === 'ENTER') {
+
+        return;
+    }
+    addLetter(key);
+}
+
+const addLetter = (letter) => {
+    if (currentTile < 5 && currentRow < 6) {
+        const tile = document.getElementById('guessRow-' + currentRow + '-tile-' + currentTile);
+        tile.textContent = letter;
+        guessRows[currentRow][currentTile] = letter;
+        tile.setAttribute('data', letter);
+        currentTile++;
+    }
+}
+
+const deleteLetter = (letter) => {
+    if (currentTile > 0) {
+        currentTile--;
+        const tile = document.getElementById('guessRow-' + currentRow + '-tile-' + currentTile);
+        tile.textContent = '';
+        guessRows[currentRow][currentTile] = '';
+        tile.setAttribute('data', '');
+    }
+}

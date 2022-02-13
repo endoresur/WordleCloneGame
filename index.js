fe3453bf9934974f9883b7f8faf89f1d7e8a7046
endoresur <mlhv2lv@gmail.com>
2022-02-13 13:36:54 +0800

    Add line wrapping and match colorization

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const tileDisplay = document.querySelector('.tile-container');
 const keyboard = document.querySelector('.key-container');
+const messageDisplay = document.querySelector('.message-container');
 
 const wordle = 'SUPER';
 
 const keys = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A',
-    'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'ENTER', 'Z',
-    'X', 'C', 'V', 'B', 'N', 'M', '«'];
+              'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'ENTER', 'Z',
+              'X', 'C', 'V', 'B', 'N', 'M', '«'
+];
 
 const guessRows = [
     ['', '', '', '', ''],
@@ -18,6 +20,7 @@ const guessRows = [
 
 let currentRow = 0;
 let currentTile = 0;
+let isGameOver = false;
 
 guessRows.forEach((guessRow, rowIndex) => {
     const rowElement = document.createElement('div');
@@ -45,7 +48,7 @@ const handleClick = (key) => {
         return;
     }
     if (key === 'ENTER') {
-
+        checkRow();
         return;
     }
     addLetter(key);
@@ -69,4 +72,54 @@ const deleteLetter = (letter) => {
         guessRows[currentRow][currentTile] = '';
         tile.setAttribute('data', '');
     }
+}
+
+const checkRow = () => {
+    const guess = guessRows[currentRow].join('');
+
+    if (currentTile > 4) {
+        console.log('guess is ' + guess + ', wordle is ' + wordle);
+        flipTile();
+        if (guess === wordle) {
+            showMessage('Great!');
+            isGameOver = true;
+            return;
+        }
+        else {
+            if (currentRow >= 5) {
+                isGameOver = true;
+                showMessage('Game Over')
+                return;
+            }
+            if (currentRow < 5) {
+                currentRow++;
+                currentTile = 0;
+            }
+        }
+    }
+}
+
+const showMessage = (message) => {
+    const messageElement = document.createElement('p');
+    messageElement.textContent = message;
+    messageDisplay.append(messageElement);
+    setTimeout(() => messageElement.removeChild(messageElement), 2000);
+    
+}
+
+const flipTile = () => {
+    const rowTiles = document.querySelector('#guessRow-' + currentRow).childNodes;
+    rowTiles.forEach((tile, index) => {
+        const dataLetter = tile.getAttribute('data');
+        if (dataLetter === wordle[index]) {
+            tile.classList.add('green-overlay');
+        } 
+        else if (wordle.includes(dataLetter)) {
+            tile.classList.add('yellow-overlay');
+        }
+        else {
+            tile.classList.add('grey-overlay');
+        }
+
+    })
 }
